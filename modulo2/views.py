ed48b16e3c9abf06de99681dc24ca55fadb8fb32
gutexp <gutexp@hotmail.com>
2018-09-29 20:02:06 -0300

    Add cubic spline method

--- modulo2/views.py
+++ modulo2/views.py
@@ -101,3 +101,71 @@ def calculaGaussJordan(request):
     print('solucoes')
     print(solucoes)
     return HttpResponse(json.dumps({ 'solution': solucoes, 'extended': matrizExtendida }), content_type="application/json")
+
+def cubic_spline(x, y):
+    """
+    Parametros são lista de floats
+    ----------
+    Retorna uma lista de uma lista de floats
+    """
+    n = len(x) - 1
+    h = [x[i + 1] - x[i] for i in range(n)]
+    al = [3 * ((y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1]) for i in range(1, n)]
+    al.insert(0, 0)
+
+    l = [1] * (n + 1)
+    u = [0] * (n + 1)
+    z = [0] * (n + 1)
+    for i in range(1, n):
+        l[i] = 2 * (x[i + 1] - x[i - 1]) - h[i - 1] * u[i - 1]
+        u[i] = h[i] / l[i]
+        z[i] = (al[i] - h[i - 1] * z[i - 1]) / l[i]
+
+    b = [0] * (n + 1)
+    c = [0] * (n + 1)
+    d = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):  # for i in reversed(range(n)):
+        c[i] = z[i] - u[i] * c[i + 1]
+        b[i] = (y[i + 1] - y[i]) / h[i] - h[i] * (c[i + 1] + 2 * c[i]) / 3
+        d[i] = (c[i + 1] - c[i]) / (3 * h[i])
+    return [y, b, c, d]
+
+
+def processingSpline():
+
+    # entradas
+    def f(x): #função
+        return math.e ** x
+
+    interval = 3
+    x = [i for i in range(interval + 1)] #lista de floats 
+    y = [f(i) for i in range(interval + 1)] #lista de floats 
+
+    # processamento de x e y
+    a = cubic_spline(x, y)
+
+    # Preparando os dados para plotagem da spline
+    points_per_interval = 5
+    xs = []
+    ys = []
+    for i in range(3):
+        xs.append(np.linspace(i, i + 1, points_per_interval))
+        ys.append([a[0][i] +
+                   a[1][i] * (xs[i][k] - i) +
+                   a[2][i] * (xs[i][k] - i) ** 2 +
+                   a[3][i] * (xs[i][k] - i) ** 3
+                   for k in range(points_per_interval)])
+
+    # Preparando os dados para plotagem da função dada
+    x = np.linspace(0, 3, interval * points_per_interval - (interval - 1))
+    y = [f(x[i]) for i in range(len(x))]
+
+    """
+    Exemplo de plotagem
+    plt.plot(x, y, 'k.-', xs[0], ys[0], 'r.--', xs[1], ys[1], 'g.--', xs[2], ys[2], 'b.--')
+
+    plt.title('Spline Cubica')
+    plt.xlabel('x')
+    plt.ylabel('e^x')
+    plt.show()
+    """
